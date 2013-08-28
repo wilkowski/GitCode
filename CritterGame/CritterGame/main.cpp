259c73aa1e577b4f7fa16540de182e7fe4be8096
Aaron <wilkowski@alumni.caltech.edu>
2013-08-27 21:40:15 -0700

    added enemy, bug fixes, top bar, cleaner code

--- CritterGame/CritterGame/main.cpp
+++ CritterGame/CritterGame/main.cpp
@@ -2,14 +2,17 @@
 #include "StdAfx.h"
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
+#include <windows.h> //used for sleep only
 #include <iostream>
 #include <string>
 #include <list>
+#include <cmath> 
 //#include <conio.h>
 
-#define XSIZE 20
-#define YSIZE 10
-#define TILE_SIZE 16
+#define XSIZE 20.0
+#define YSIZE 20.0
+#define TILE_SIZE 16.0
+#define HEADER_SIZE 20.0
 
 using namespace std;
 
@@ -44,11 +47,18 @@ class Location{
     Location SubVec(dirVec vec){
         return Location(x-vec.x, y-vec.y);
     }
+	bool operator==(Location l){
+		return (x == l.x && y == l.y);
+	}
+	bool operator!=(Location l){
+		return !(x == l.x && y == l.y);
+	}
 };
 
 class Entity{
     //iterate through these to remove refs
     list<list<Entity *>*> listRefs;
+	Location initialLoc;
 public:
     int symbol;
     Location loc;
@@ -56,6 +66,7 @@ public:
     Entity(int s, Location l){
         symbol = s;
         loc = l;
+		initialLoc = loc;
     }
     void AddToList(list<Entity> *l){
 		//l->insert(l->end(), this);
@@ -63,6 +74,10 @@ public:
         //first add self to l and make an iterator i pointing to it
         //add iterator to iterator list
     }
+	void Reset(){
+		loc = initialLoc;
+		
+	}
     ~Entity(){
         //iterator through iterator list
         //for each delete self from list
@@ -71,22 +86,34 @@ public:
 
 class GameBoard{
     int *board; 
-    list<Entity> entityList;
+    list<Entity *> entityList;
     
 public:
     int xSize;
     int ySize;
-    GameBoard(){
-        xSize = XSIZE;
-        ySize = YSIZE;
-        board = new int[XSIZE*YSIZE];
-        
-        for(int i = 0; i<xSize; i++){
+	void ResetBoard(){
+		for(int i = 0; i<xSize; i++){
             for(int j = 0; j < ySize; j++){
                 *SpotRef(i,j) = (rand()%2); //Randomize initial board state
             }
         }
-        *SpotRef(0,0) = 2;
+		*SpotRef(0,1) = 0;
+		*SpotRef(1,0) = 0;
+		*SpotRef(1,1) = 0; //hopefully avoids starting off stuck
+		for (list<Entity *>::iterator iterator = entityList.begin(); iterator != entityList.end(); ++iterator){
+			(*iterator)->Reset();
+			*SpotRef((*iterator)->loc) = (*iterator)->symbol;
+			cout << "set loc " << (*iterator)->loc.x << "," << (*iterator)->loc.y << " symbol " << (*iterator)->symbol << endl;
+		}
+	}
+
+    GameBoard(){
+        xSize = XSIZE;
+        ySize = YSIZE;
+        board = new int[XSIZE*YSIZE];
+        ResetBoard();
+
+        //*SpotRef(0,0) = 2;
         //dispVals = ;
         //dispVals 
     }
@@ -110,7 +137,7 @@ public:
         return (l.x >= 0 && l.x < xSize && l.y>=0 && l.y < ySize);
     }
     //attempt to move a block to an adjacent space, no restrictions
-    bool TryMoveBlock(Location fromLoc, dirVec dir){
+    bool TryMoveBlock(Location fromLoc, dirVec dir, bool entity){
         
         Location newLoc = fromLoc.AddVec(dir);
         if(!ValidLocation(fromLoc) || !ValidLocation(newLoc)){
@@ -119,9 +146,15 @@ public:
         if(SpotVal(fromLoc) == 0){  //is there a block at location?
             return false;
         }
-        if(SpotVal(newLoc) != 0){
-            return false;
-        }
+		if(entity){
+			if(SpotVal(newLoc) == 1){ //trying to move into a block
+				return false;
+			}
+		}else{
+			if(SpotVal(newLoc) != 0){ //is new location empty
+				return false;
+			}
+		}
         //TODO: check if block is an entity (via value)
         //If yes then update entity's location
         int blockVal = SpotVal(fromLoc);
@@ -129,29 +162,34 @@ public:
         *SpotRef(fromLoc) = 0;//update old location
         return true;
     }
-    bool TryMoveEntity(Entity &ent, dirVec dir, bool canPush, bool canPull){
-		cout << "move try" << endl;
-        Location pullFrom = ent.loc.SubVec(dir);
-        Location pushFrom = ent.loc.AddVec(dir);
-		cout << "from" << pushFrom.x << "," << pushFrom.y << endl;
+    bool TryMoveEntity(Entity *ent, dirVec dir, bool canPush, bool canPull){
+		//cout << "move try" << endl;
+        Location pullFrom = ent->loc.SubVec(dir);
+        Location pushFrom = ent->loc.AddVec(dir);
+		//cout << "from" << pushFrom.x << "," << pushFrom.y << endl;
         if(canPush){//try moving pushed block
-            TryMoveBlock(pushFrom, dir); //does't matter if successful
+            TryMoveBlock(pushFrom, dir, false); //does't matter if successful
         }
         bool success = true;
-        success = TryMoveBlock(ent.loc, dir);  //try moving self
+        success = TryMoveBlock(ent->loc, dir, true);  //try moving self
         if(!success){ 
             return false;
         }
 		
-        ent.loc = pushFrom; //might be done in move sec later
-		cout << "ent" << ent.loc.x << "," << ent.loc.y << endl;
+        ent->loc = pushFrom; //might be done in move sec later
+		//cout << "ent" << ent.loc.x << "," << ent.loc.y << endl;
         if(canPull){//is pulling?
-            TryMoveBlock(pullFrom,dir); //try moving pulled block
+            TryMoveBlock(pullFrom,dir, false); //try moving pulled block
         }
-		cout << "\n" << "Move success";
+		//cout << "\n" << "Move success";
         return true;
     }
     void DisplayBoard(sf::RenderWindow &window){
+		sf::RectangleShape rectangle(sf::Vector2f(120, 50));
+		rectangle.setSize(sf::Vector2f(XSIZE*TILE_SIZE, HEADER_SIZE));
+		rectangle.setFillColor(sf::Color::Blue);
+		window.draw(rectangle);
+		sf::Font font;
 		sf::CircleShape shape(8.f);
         for(int i = 0; i < YSIZE; i++){
             for(int j = 0; j < XSIZE; j++){
@@ -165,35 +203,92 @@ public:
 					case 2:
 						shape.setFillColor(sf::Color::Yellow);
 						break;
-					default:
+					case 3:
 						shape.setFillColor(sf::Color::Red);
 						break;
+					case 4:
+						shape.setFillColor(sf::Color::Magenta);
+						break;
+					default:
+						shape.setFillColor(sf::Color::White); //not used
+						break;
 				}
-				shape.setPosition(sf::Vector2f(j*TILE_SIZE, TILE_SIZE * (YSIZE-1) - i*TILE_SIZE));
+				shape.setPosition(sf::Vector2f(j*TILE_SIZE, HEADER_SIZE + TILE_SIZE * (YSIZE-1) - i*TILE_SIZE));
 				window.draw(shape);
             }
         }
     }
-	void AddEntity(Entity ent){
+	void AddEntity(Entity *ent){
 		entityList.insert(entityList.end(), ent);
-		*SpotRef(ent.loc) = ent.symbol;
+		*SpotRef(ent->loc) = ent->symbol;
 	}
     ~GameBoard(){
         delete board;
     }
 };
 
+bool moveMonster(GameBoard *primaryBoard, Entity *monster, Entity *player){
+	if(monster->loc == player->loc){
+		primaryBoard->ResetBoard();
+	}else{
+		bool oneDirection = (monster->loc.x == player->loc.x || monster->loc.y == player->loc.y);
+		int i = 0;
+		dirVec possibleMoves[2];
+		int weight[2];
+		int absx = abs(monster->loc.x-player->loc.x);
+		if(monster->loc.x < player->loc.x){
+			possibleMoves[i] = dirVec(1,0);
+			weight[i] = absx;
+			i++;
+		}else if(monster->loc.x > player->loc.x){
+			possibleMoves[i] = dirVec(-1,0);
+			weight[i] = absx;
+			i++;
+		}
+		int absy = abs(monster->loc.y-player->loc.y);
+		if(monster->loc.y < player->loc.y){
+			possibleMoves[i] = dirVec(0,1);
+			weight[i] = absy;
+			i++;
+		}else if(monster->loc.y > player->loc.y){
+			possibleMoves[i] = dirVec(0,-1);
+			weight[i] = absy;
+			i++;
+		}
+		int choice = 0;
+		if(!oneDirection){
+			if (rand()%(weight[0]+weight[1]) >= weight[0]){
+				choice = 1;
+			}
+		}
+		bool success = primaryBoard->TryMoveEntity(monster, possibleMoves[choice], true, false);
+		if(!oneDirection && ! success){
+			return primaryBoard->TryMoveEntity(monster, possibleMoves[1-choice], true, false); //try other way
+		}
+		return success;
+	}
+}
+
 int main()
 {
-	Location *playerLocation = new Location(0,0);
-    
+	int difficulty = 3;
+	unsigned int moveCounter = 0;
+	Location playerStart = Location(0,0);
+    Location monsterStart = Location(XSIZE-2, YSIZE-2);
+	Location goalLocation = Location(XSIZE-1, YSIZE-1);
     GameBoard *primaryBoard = new GameBoard();
-
-	Entity player = Entity(2, Location(0,0));
+	Entity *player = new Entity(2, playerStart);
+	Entity *monster = new Entity(3, monsterStart);
+	Entity *goal = new Entity(4, goalLocation);
 	primaryBoard->AddEntity(player);
+	primaryBoard->AddEntity(monster);
+	primaryBoard->AddEntity(goal);
+	
     
-    sf::RenderWindow window(sf::VideoMode(TILE_SIZE * XSIZE, TILE_SIZE * YSIZE), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(TILE_SIZE * XSIZE, TILE_SIZE * YSIZE + HEADER_SIZE), "Critter Game!");
     sf::CircleShape shape(8.f);
+
+	Location lastPlayerLocation = playerStart;
 
     while (window.isOpen())
     {
@@ -211,18 +306,37 @@ int main()
 				// key pressed
 				case sf::Event::KeyPressed:
 					
-					if (event.key.code == sf::Keyboard::S){
-						primaryBoard->TryMoveEntity(player, dirVec(0,-1), true, shouldPull);
-					}
+					
 					if (event.key.code == sf::Keyboard::W){
-						primaryBoard->TryMoveEntity(player, dirVec(0,1), true, shouldPull);
+						primaryBoard->TryMoveEntity(player, dirVec(0,1), true, true);
+					} else if (event.key.code == sf::Keyboard::A){
+						primaryBoard->TryMoveEntity(player, dirVec(-1,0), true, true);
+					} else if (event.key.code == sf::Keyboard::S){
+						primaryBoard->TryMoveEntity(player, dirVec(0,-1), true, true);
+					} else if (event.key.code == sf::Keyboard::D){
+						primaryBoard->TryMoveEntity(player, dirVec(1,0), true, true);
+					} else if (event.key.code == sf::Keyboard::Escape){
+						player->loc = playerStart;
+						//monster.loc = monsterStart;
+						primaryBoard->ResetBoard();
+						cout << "mon " << monster->loc.x << "," << monster->loc.y << endl;
 					}
-					if (event.key.code == sf::Keyboard::A){
-						primaryBoard->TryMoveEntity(player, dirVec(-1,0), true, shouldPull);
+					if(lastPlayerLocation != player->loc){ // move monster
+						bool moved = moveMonster(primaryBoard, monster, player);
+						moveCounter++;
+						if (moved && (moveCounter %difficulty != 0)){
+							window.clear();
+							primaryBoard->DisplayBoard(window);
+							window.display();
+							Sleep(200);
+							moveMonster(primaryBoard, monster, player);
+						}
 					}
-					if (event.key.code == sf::Keyboard::D){
-						primaryBoard->TryMoveEntity(player, dirVec(1,0), true, shouldPull);
+					if(player->loc == monster->loc || player->loc == goalLocation){
+						difficulty++;
+						primaryBoard->ResetBoard();
 					}
+					lastPlayerLocation = player->loc;
 					break;
 				// we don't process other types of events
 				default:
